06df11607345bc50240c9a67e5c7345aef8110ff
Первушина Анна <annafirst14@mail.ru>
2023-09-07 18:01:14 +0500

    дом зад 8

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,65 @@
 //Задача № 1
-function cachingDecoratorNew(func) {
+//function cachingDecoratorNew(func) {
   
+//}
+
+//Задача № 2
+//function debounceDecoratorNew(func, delay) {
+  
+//}
+function cachingDecoratorNew(func) {
+  const cache = []
+
+  function wrapper(...args) {
+    const hash = md5(JSON.stringify(args))
+    const cachedItem = cache.find((item) => item.hash === hash)
+
+    if (cachedItem) {
+      console.log("Из кэша: " + cachedItem.value)
+      return "Из кэша: " + cachedItem.value
+    }
+
+    const result = func(...args)
+    cache.push({ hash, value: result })
+
+    if (cache.length > 5) {
+      cache.shift()
+    }
+
+    console.log("Вычисляем: " + result)
+    return "Вычисляем: " + result
+  }
+
+  return wrapper
 }
 
 //Задача № 2
 function debounceDecoratorNew(func, delay) {
-  
+  let timeoutId
+  let count = 0
+  let allCount = 0
+
+  function wrapper(...args) {
+    clearTimeout(timeoutId)
+
+    if (!timeoutId) {
+      func(...args)
+      count++
+    }
+
+    timeoutId = setTimeout(() => {
+      func(...args)
+      timeoutId = undefined
+    }, delay)
+
+    allCount++
+
+    wrapper.count = count
+    wrapper.allCount = allCount
+  }
+
+  wrapper.count = 0
+  wrapper.allCount = 0
+
+  return wrapper
 }
