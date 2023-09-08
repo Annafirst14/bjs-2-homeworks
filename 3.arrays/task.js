aa3fbba21a9ce521f05711465548b43442c9ef81
Первушина Анна <annafirst14@mail.ru>
2023-09-08 23:59:40 +0500

    дом зад 3,испр

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,9 +1,7 @@
 
 function compareArrays(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false
-  }
 
+return arr1.length === arr2.length && arr1.every((item, index) => item === arr2[index]);
   return arr1.every((element, index) => element === arr2[index])
 }
 
