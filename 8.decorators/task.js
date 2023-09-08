53aea045e52a037927c905b56ed1bcfb068cc2b7
Первушина Анна <annafirst14@mail.ru>
2023-09-08 18:41:06 +0500

    дом зад 8,8

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,12 +1,5 @@
 //Задача № 1
-//function cachingDecoratorNew(func) {
-  
-//}
 
-//Задача № 2
-//function debounceDecoratorNew(func, delay) {
-  
-//}
 function cachingDecoratorNew(func) {
   const cache = []
 
